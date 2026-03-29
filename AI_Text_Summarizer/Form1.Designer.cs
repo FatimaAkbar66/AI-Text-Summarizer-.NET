b9bf877ec6175ee2366524855b5603ff5ed14f74
Fatima Akbar <fatimah9286@gmail.com>
2026-03-29 12:56:35 -0700

    feat: Make paragraph and bullet point toggle buttons fully functional

--- AI_Text_Summarizer/Form1.Designer.cs
+++ AI_Text_Summarizer/Form1.Designer.cs
@@ -6,8 +6,14 @@
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.Button btnSummarize;
-        private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Panel pnlCentralCard;
+        private System.Windows.Forms.Panel pnlTopBar;
+        private System.Windows.Forms.Panel pnlBottomBar;
+        private System.Windows.Forms.Label lblInputWordCount;
+        private System.Windows.Forms.Label lblOutputWordCount;
+        private System.Windows.Forms.Button btnParagraph;
+        private System.Windows.Forms.Button btnBulletPoints;
 
         protected override void Dispose(bool disposing)
         {
@@ -23,84 +29,176 @@
             this.txtInput = new System.Windows.Forms.TextBox();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.btnSummarize = new System.Windows.Forms.Button();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.pnlCentralCard = new System.Windows.Forms.Panel();
+            this.pnlTopBar = new System.Windows.Forms.Panel();
+            this.btnParagraph = new System.Windows.Forms.Button();
+            this.btnBulletPoints = new System.Windows.Forms.Button();
+            this.pnlBottomBar = new System.Windows.Forms.Panel();
+            this.lblInputWordCount = new System.Windows.Forms.Label();
+            this.lblOutputWordCount = new System.Windows.Forms.Label();
+            this.pnlCentralCard.SuspendLayout();
+            this.pnlTopBar.SuspendLayout();
+            this.pnlBottomBar.SuspendLayout();
             this.SuspendLayout();
-
-            // lblTitle
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(190, 41, 236); // Neon Purple
-            this.lblTitle.Location = new System.Drawing.Point(30, 20);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(250, 41);
-            this.lblTitle.Text = "AI Text Summarizer";
-
+            // 
             // txtInput
-            this.txtInput.BackColor = System.Drawing.Color.FromArgb(30, 30, 36);
-            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtInput.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtInput.ForeColor = System.Drawing.Color.White;
-            this.txtInput.Location = new System.Drawing.Point(35, 80);
+            // 
+            this.txtInput.BackColor = System.Drawing.Color.Lavender;
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInput.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.txtInput.Location = new System.Drawing.Point(17, 81);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
-            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtInput.Size = new System.Drawing.Size(400, 250);
-            this.txtInput.Text = "Paste your long article or text here...";
-
+            this.txtInput.Size = new System.Drawing.Size(622, 376);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Paste your text here...";
+            // 
             // txtOutput
-            this.txtOutput.BackColor = System.Drawing.Color.FromArgb(30, 30, 36);
-            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtOutput.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtOutput.ForeColor = System.Drawing.Color.FromArgb(255, 140, 0); // Neon Orange text for output
-            this.txtOutput.Location = new System.Drawing.Point(460, 80);
+            // 
+            this.txtOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOutput.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtOutput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.txtOutput.Location = new System.Drawing.Point(646, 81);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.ReadOnly = true;
-            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.Size = new System.Drawing.Size(400, 250);
-            this.txtOutput.Text = "AI Summary will appear here...";
-
+            this.txtOutput.Size = new System.Drawing.Size(622, 376);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "Your summary will appear here...";
+            // 
             // btnSummarize
-            this.btnSummarize.BackColor = System.Drawing.Color.FromArgb(190, 41, 236); // Neon Purple
+            // 
+            this.btnSummarize.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.btnSummarize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSummarize.FlatAppearance.BorderSize = 0;
             this.btnSummarize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSummarize.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSummarize.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
             this.btnSummarize.ForeColor = System.Drawing.Color.White;
-            this.btnSummarize.Location = new System.Drawing.Point(35, 350);
+            this.btnSummarize.Location = new System.Drawing.Point(606, 652);
             this.btnSummarize.Name = "btnSummarize";
-            this.btnSummarize.Size = new System.Drawing.Size(825, 50);
-            this.btnSummarize.Text = "Generate Summary";
+            this.btnSummarize.Size = new System.Drawing.Size(169, 45);
+            this.btnSummarize.TabIndex = 0;
+            this.btnSummarize.Text = "Summarize";
             this.btnSummarize.UseVisualStyleBackColor = false;
             this.btnSummarize.Click += new System.EventHandler(this.btnSummarize_Click);
-
-            // lblStatus
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblStatus.ForeColor = System.Drawing.Color.Silver;
-            this.lblStatus.Location = new System.Drawing.Point(35, 415);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(100, 23);
-            this.lblStatus.Text = "Status: Ready";
-
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeader.Font = new System.Drawing.Font("Franklin Gothic Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblHeader.Location = new System.Drawing.Point(360, 30);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(773, 61);
+            this.lblHeader.TabIndex = 2;
+            this.lblHeader.Text = "Summarize any text with One Click";
+            // 
+            // pnlCentralCard
+            // 
+            this.pnlCentralCard.BackColor = System.Drawing.Color.White;
+            this.pnlCentralCard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCentralCard.Controls.Add(this.txtInput);
+            this.pnlCentralCard.Controls.Add(this.txtOutput);
+            this.pnlCentralCard.Controls.Add(this.pnlTopBar);
+            this.pnlCentralCard.Controls.Add(this.pnlBottomBar);
+            this.pnlCentralCard.Location = new System.Drawing.Point(45, 110);
+            this.pnlCentralCard.Name = "pnlCentralCard";
+            this.pnlCentralCard.Size = new System.Drawing.Size(1283, 536);
+            this.pnlCentralCard.TabIndex = 1;
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.BackColor = System.Drawing.Color.White;
+            this.pnlTopBar.Controls.Add(this.btnParagraph);
+            this.pnlTopBar.Controls.Add(this.btnBulletPoints);
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.Size = new System.Drawing.Size(1281, 63);
+            this.pnlTopBar.TabIndex = 2;
+            // 
+            // btnParagraph
+            // 
+            this.btnParagraph.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnParagraph.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnParagraph.ForeColor = System.Drawing.Color.White;
+            this.btnParagraph.Location = new System.Drawing.Point(17, 10);
+            this.btnParagraph.Name = "btnParagraph";
+            this.btnParagraph.Size = new System.Drawing.Size(101, 37);
+            this.btnParagraph.TabIndex = 0;
+            this.btnParagraph.Text = "Paragraph";
+            this.btnParagraph.UseVisualStyleBackColor = false;
+            this.btnParagraph.Click += new System.EventHandler(this.btnParagraph_Click_1);
+            // 
+            // btnBulletPoints
+            // 
+            this.btnBulletPoints.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnBulletPoints.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBulletPoints.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.btnBulletPoints.Location = new System.Drawing.Point(124, 10);
+            this.btnBulletPoints.Name = "btnBulletPoints";
+            this.btnBulletPoints.Size = new System.Drawing.Size(117, 37);
+            this.btnBulletPoints.TabIndex = 1;
+            this.btnBulletPoints.Text = "Bullet Points";
+            this.btnBulletPoints.UseVisualStyleBackColor = false;
+            this.btnBulletPoints.Click += new System.EventHandler(this.btnBulletPoints_Click_1);
+            // 
+            // pnlBottomBar
+            // 
+            this.pnlBottomBar.BackColor = System.Drawing.Color.White;
+            this.pnlBottomBar.Controls.Add(this.lblInputWordCount);
+            this.pnlBottomBar.Controls.Add(this.lblOutputWordCount);
+            this.pnlBottomBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBottomBar.Location = new System.Drawing.Point(0, 474);
+            this.pnlBottomBar.Name = "pnlBottomBar";
+            this.pnlBottomBar.Size = new System.Drawing.Size(1281, 60);
+            this.pnlBottomBar.TabIndex = 3;
+            // 
+            // lblInputWordCount
+            // 
+            this.lblInputWordCount.AutoSize = true;
+            this.lblInputWordCount.ForeColor = System.Drawing.Color.Gray;
+            this.lblInputWordCount.Location = new System.Drawing.Point(17, 15);
+            this.lblInputWordCount.Name = "lblInputWordCount";
+            this.lblInputWordCount.Size = new System.Drawing.Size(72, 20);
+            this.lblInputWordCount.TabIndex = 0;
+            this.lblInputWordCount.Text = "Words: 0";
+            // 
+            // lblOutputWordCount
+            // 
+            this.lblOutputWordCount.AutoSize = true;
+            this.lblOutputWordCount.ForeColor = System.Drawing.Color.Gray;
+            this.lblOutputWordCount.Location = new System.Drawing.Point(1125, 15);
+            this.lblOutputWordCount.Name = "lblOutputWordCount";
+            this.lblOutputWordCount.Size = new System.Drawing.Size(72, 20);
+            this.lblOutputWordCount.TabIndex = 1;
+            this.lblOutputWordCount.Text = "Words: 0";
+            // 
             // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(18, 18, 18); // Deep Dark background
-            this.ClientSize = new System.Drawing.Size(900, 460);
-            this.Controls.Add(this.lblStatus);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(1385, 720);
             this.Controls.Add(this.btnSummarize);
-            this.Controls.Add(this.txtOutput);
-            this.Controls.Add(this.txtInput);
-            this.Controls.Add(this.lblTitle);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
+            this.Controls.Add(this.pnlCentralCard);
+            this.Controls.Add(this.lblHeader);
+            this.MinimumSize = new System.Drawing.Size(1347, 720);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AI Text Summarizer";
+            this.pnlCentralCard.ResumeLayout(false);
+            this.pnlCentralCard.PerformLayout();
+            this.pnlTopBar.ResumeLayout(false);
+            this.pnlBottomBar.ResumeLayout(false);
+            this.pnlBottomBar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
     }
 }
--- AI_Text_Summarizer/Form1.cs
+++ AI_Text_Summarizer/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +11,47 @@ namespace AI_Text_Summarizer
 {
     public partial class Form1 : Form
     {
+        // Keep track of the selected summarization format
+        private string summaryFormat = "Paragraph";
+
         public Form1()
         {
             InitializeComponent();
+
+            // 1. Force the window to open at a large, specific size (Width: 1250, Height: 750)
+            this.Size = new Size(1050, 520);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // 2. This keeps your central card perfectly centered no matter what!
+            pnlCentralCard.Left = (this.ClientSize.Width - pnlCentralCard.Width) / 2;
+            pnlCentralCard.Top = (this.ClientSize.Height - pnlCentralCard.Height) / 2;
+
+            // Wire up the placeholder, word count, and format button events
+            txtInput.Enter += TxtInput_Enter;
+            txtInput.Leave += TxtInput_Leave;
+            txtInput.TextChanged += TxtInput_TextChanged;
+
+            btnParagraph.Click += BtnParagraph_Click;
+            btnBulletPoints.Click += BtnBulletPoints_Click;
+
+            // Set initial active button styling
+            SetActiveButtonStyling();
         }
 
         private async void btnSummarize_Click(object sender, EventArgs e)
         {
             string inputText = txtInput.Text.Trim();
 
-            // Validate input so the user doesn't send empty text
-            if (string.IsNullOrEmpty(inputText) || inputText == "Paste your long article or text here...")
+            // Validate input
+            if (string.IsNullOrEmpty(inputText) || inputText == "Paste your text here...")
             {
                 MessageBox.Show("Please enter some text to summarize.", "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
             // Update UI to show loading state
-            lblStatus.Text = "Status: Processing with Gemini AI... Please wait.";
             btnSummarize.Enabled = false;
+            btnSummarize.Text = "Processing...";
             txtOutput.Text = "Generating summary...";
 
             try
@@ -35,38 +59,45 @@ namespace AI_Text_Summarizer
                 // Call the API and update the output box
                 string summary = await GetSummaryFromAI(inputText);
                 txtOutput.Text = summary;
-                lblStatus.Text = "Status: Summarization Complete!";
+
+                // Update output word count
+                lblOutputWordCount.Text = $"Words: {CountWords(summary)}";
             }
             catch (Exception ex)
             {
-                // Fulfills the "Proper error handling" assignment requirement
                 MessageBox.Show($"An error occurred: {ex.Message}", "API Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                lblStatus.Text = "Status: Error occurred.";
                 txtOutput.Text = "Failed to generate summary.";
             }
             finally
             {
                 // Re-enable the button when done
                 btnSummarize.Enabled = true;
+                btnSummarize.Text = "Summarize";
             }
         }
 
         private async Task<string> GetSummaryFromAI(string text)
         {
             // 1. Put your Gemini API key here
-            string geminiApiKey = "API KEY";
+            string geminiApiKey = "API_KEY";
 
-            // 2. We use the blazing-fast Gemini 2.0 Flash model
+            // 2. Stable gemini-2.5-flash
             string url = $"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={geminiApiKey}";
+
+            // 3. We change the instruction sent to Gemini based on the button selected!
+            string promptInstruction = "Summarize the following text concisely in a short paragraph: ";
+            if (summaryFormat == "Bullet Points")
+            {
+                promptInstruction = "Summarize the following text using clear, concise bullet points (use '-' for bullets): ";
+            }
 
             using (HttpClient client = new HttpClient())
             {
-                // We create the exact JSON structure Google Gemini expects
                 var requestBody = new
                 {
                     contents = new[]
                     {
-                        new { parts = new[] { new { text = "Summarize the following text concisely in a short paragraph: " + text } } }
+                        new { parts = new[] { new { text = promptInstruction + text } } }
                     }
                 };
 
@@ -80,17 +111,116 @@ namespace AI_Text_Summarizer
 
                 if (response.IsSuccessStatusCode)
                 {
-                    // Parse Google's response to extract just the summary text
                     JObject json = JObject.Parse(responseString);
                     string summary = json["candidates"][0]["content"]["parts"][0]["text"].ToString();
                     return summary.Trim();
                 }
                 else
                 {
-                    // Throw custom exception if the API rejects our call
                     throw new Exception($"Gemini API Error: {response.StatusCode}. Details: {responseString}");
                 }
             }
+        }
+
+        // --- BUTTON CLICK EVENTS FOR UI TOGGLE ---
+
+        private void BtnParagraph_Click(object sender, EventArgs e)
+        {
+            summaryFormat = "Paragraph";
+            SetActiveButtonStyling();
+        }
+
+        private void BtnBulletPoints_Click(object sender, EventArgs e)
+        {
+            summaryFormat = "Bullet Points";
+            SetActiveButtonStyling();
+        }
+
+        private void SetActiveButtonStyling()
+        {
+            if (summaryFormat == "Paragraph")
+            {
+                // Paragraph button becomes blue, bullet points becomes gray
+                btnParagraph.BackColor = Color.FromArgb(65, 105, 225);
+                btnParagraph.ForeColor = Color.White;
+
+                btnBulletPoints.BackColor = Color.FromArgb(240, 240, 245);
+                btnBulletPoints.ForeColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                // Bullet Points button becomes blue, paragraph becomes gray
+                btnBulletPoints.BackColor = Color.FromArgb(65, 105, 225);
+                btnBulletPoints.ForeColor = Color.White;
+
+                btnParagraph.BackColor = Color.FromArgb(240, 240, 245);
+                btnParagraph.ForeColor = Color.FromArgb(60, 60, 60);
+            }
+        }
+
+        // --- HELPER EVENTS FOR MODERN UX ---
+
+        private void TxtInput_Enter(object sender, EventArgs e)
+        {
+            if (txtInput.Text == "Paste your text here...")
+            {
+                txtInput.Text = "";
+                txtInput.ForeColor = Color.FromArgb(25, 25, 40);
+            }
+        }
+
+        private void TxtInput_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                txtInput.Text = "Paste your text here...";
+                txtInput.ForeColor = Color.FromArgb(120, 120, 120);
+            }
+        }
+
+        private void TxtInput_TextChanged(object sender, EventArgs e)
+        {
+            if (txtInput.Text != "Paste your text here...")
+            {
+                lblInputWordCount.Text = $"Words: {CountWords(txtInput.Text)}";
+            }
+            else
+            {
+                lblInputWordCount.Text = "Words: 0";
+            }
+        }
+
+        private int CountWords(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return 0;
+
+            string[] words = text.Split(new[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+
+        // --- BACKGROUND PAINTING ---
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.FromArgb(215, 245, 245),
+                Color.FromArgb(245, 225, 250),
+                45F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void btnParagraph_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBulletPoints_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
